760a6c9895113a06622a0defcba6b4f8e0936ef3
howardbdev <howarddevennish@gmail.com>
2020-04-23 10:22:24 -0400

    change App to class for state

--- react-intro-cars/src/App.js
+++ react-intro-cars/src/App.js
@@ -3,13 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Cars from './Cars.js'
 
-function App() {
-  return (
-    <div className="App">
-    <h1>🚗 React Cars 🚙</h1>
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+      <h1>🚗 React Cars 🚙</h1>
       <Cars />
-    </div>
-  );
+      </div>
+    );
+
+  }
 }
 
 export default App;
