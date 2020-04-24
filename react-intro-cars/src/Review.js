cdd2da2bab4624889b6dd543f13a2f811aae1124
howardbdev <howarddevennish@gmail.com>
2020-04-23 21:49:24 -0400

    update Review component

--- react-intro-cars/src/Review.js
+++ react-intro-cars/src/Review.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Review = (props) => {
 
-  return props.review ?
-    <div>
-      <h3>{props.review.rating} Stars</h3>
-      <p>{props.review.content}</p>
-    </div> :
-    <h5>Loading Reviews...</h5>
+  return (
+    props.review ?
+      <div>
+        <h3>{props.review.rating} Stars</h3>
+        <p>{props.review.content}</p>
+      </div> :
+      <h5>Loading Reviews...</h5>
+  )
 }
 
 export default Review
